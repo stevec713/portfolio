91bcf3715e528a09a95cf18d18400dd203ed5340
Stephen <scareylps@gmail.com>
2025-11-30 15:49:26 -0500

    updated css

---
+++ script.js
@@ -0,0 +1,31 @@
+let clickableSection = document.getElementById('clickableSection');
+let skills = document.getElementById('skills');
+let contact = document.getElementById('contact');
+let main = document.getElementById('main');
+let header = document.querySelector('header');
+let projects = document.querySelector('#projects > div');
+let about = document.querySelector('nav [href="#about"]');
+let work = document.querySelector('nav [href="#work"]');
+
+function toggleVisibility() {
+    clickableSection.classList.toggle('left');
+    header.classList.toggle('hidden');
+    projects.classList.toggle('hidden');
+    skills.classList.toggle('hidden');
+    contact.classList.toggle('hidden');
+    main.classList.toggle('right');
+}
+
+clickableSection.addEventListener('click', toggleVisibility);
+
+about.addEventListener('click', function(){
+    if(clickableSection.classList.contains('left')){
+        toggleVisibility();
+    }
+});
+
+work.addEventListener('click', function(){
+    if(!clickableSection.classList.contains('left')){
+        toggleVisibility();
+    }
+});
